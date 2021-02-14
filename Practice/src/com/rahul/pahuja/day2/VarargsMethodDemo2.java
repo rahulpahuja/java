a1e57449855d754465abb87260a20d4ae4371b29
Rahul Pahuja <rahulpahuja2015@gmail.com>
2021-02-14 23:19:15 +0530

    Added varargs using ellipsis
    Signed-off-by: Rahul Pahuja <rahulpahuja2015@gmail.com>

---
+++ Practice/src/com/rahul/pahuja/day2/VarargsMethodDemo2.java
@@ -0,0 +1,20 @@
+package com.rahul.pahuja.day2;
+
+public class VarargsMethodDemo2 {
+    static void varargsMethod2(int... v) {
+        System.out.println("Number of Elements " + v.length);
+        for (int x : v
+        ) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        varargsMethod2(2, 3, 4, 5, 6, 7, 8, 9, 10);
+        varargsMethod2(2, 3, 4, 5, 6, 7, 8);
+        varargsMethod2(2, 3, 4, 5, 6, 7, 8, 999);
+
+
+    }
+}
