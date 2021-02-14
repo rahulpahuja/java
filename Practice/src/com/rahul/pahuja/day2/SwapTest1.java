16304731099aef1dcc857a96c9914df15b40b1b1
Rahul Pahuja <rahulpahuja2015@gmail.com>
2021-02-14 22:44:14 +0530

    Added Generic method for swap
    Signed-off-by: Rahul Pahuja <rahulpahuja2015@gmail.com>

---
+++ Practice/src/com/rahul/pahuja/day2/SwapTest1.java
@@ -0,0 +1,27 @@
+package com.rahul.pahuja.day2;
+
+public class SwapTest1 {
+    public static <T> void genericSwap(T x, T y) {
+        T temp;
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    //FIXME:Not working
+    public static void main(String[] args) {
+        Integer x = new Integer(2);
+        Integer y = new Integer(9);
+
+        System.out.println("x:" + x + " and y:" + y);
+        genericSwap(x, y);
+        System.out.println("x:" + x + " and y:" + y);
+
+
+        String a = "A";
+        String b = "B";
+        System.out.println("a:" + a + " and b:" + b);
+        genericSwap(a, b);
+        System.out.println("a:" + a + " and b:" + b);
+    }
+}
