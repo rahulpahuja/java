0640157e92dbd78f0ba86d0df32117b7476586c3
Rahul Pahuja <rahulpahuja2015@gmail.com>
2021-02-14 22:19:59 +0530

    Added printer Method which prints a List of Objects
    Signed-off-by: Rahul Pahuja <rahulpahuja2015@gmail.com>

--- Practice/src/com/rahul/pahuja/day2/GenericPrint.java
+++ Practice/src/com/rahul/pahuja/day2/GenericPrint.java
@@ -1,13 +1,25 @@
 package com.rahul.pahuja.day2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericPrint {
     public static void main(String[] args) {
         GenericPrint printer = new GenericPrint();
-        printer.genericPrint(42);
+        ArrayList<Object> objects = new ArrayList<>();
+        objects.add(42);
+        objects.add(43);
+        objects.add(432);
+        printer.genericPrint(objects);
         printer.genericPrint("Rahul");
     }
 
     <T> void genericPrint(T t) {
+        System.out.println(t);
+
+    }
+
+    <T> void genericPrint(List<T> t) {
         System.out.println(t);
     }
 }
